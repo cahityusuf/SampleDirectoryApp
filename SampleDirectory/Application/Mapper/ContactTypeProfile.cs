04459324f435d3cb70dbced8e0648ddafa5240fa
cahityusuf <cahityusuf@gmail.com>
2021-09-04 15:27:41 +0300

    Entitiy_ve_Dto_Mapping_Islemine_ait_Profile_Classlari_Tanimlandi

---
+++ SampleDirectory/Application/Mapper/ContactTypeProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abstractions.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mapper
+{
+    public class ContactTypeProfile:Profile
+    {
+        public ContactTypeProfile()
+        {
+            CreateMap<ContactType, ContactTypeDto>().ReverseMap();
+        }
+    }
+}
---
+++ SampleDirectory/Application/Mapper/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abstractions.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mapper
+{
+    public class UserProfile:Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+        }
+    }
+}
