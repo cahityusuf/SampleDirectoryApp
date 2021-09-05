4d8e29d7910a1dcbf828879f1840524524fcc101
cahityusuf <cahityusuf@gmail.com>
2021-09-06 01:07:50 +0300

    Sample_Data_Eklendi

---
+++ _User__202109060105.sql
@@ -0,0 +1,4 @@
+INSERT INTO "DirectoryUser"."User" ("Ad","Soyad","Firma") VALUES
+	 ('Cahit Yusuf','KAFADAR','ABC Firması'),
+	 ('Kerem','KAFADAR','ABC Firması'),
+	 ('Yakup','KAFADAR','ABC Firması');
